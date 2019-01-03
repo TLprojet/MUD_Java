d22277370381abae876b725e339920b61d571ccc
Tonin <toninlaborde@hotmail.fr>
2019-01-03 15:54:26 +0100

    actualize

--- src/lanceClient.java
+++ src/lanceClient.java
@@ -3,20 +3,15 @@ package src;
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
-import java.rmi.RMISecurityManager;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
-import serveur.Information;
 
 public class lanceClient {
 
   public static void main(String[] args) {
     System.out.println("Lancement du client");
-    if (System.getSecurityManager() == null) {
-      System.setSecurityManager(new RMISecurityManager());
-    }
     try {
-      Remote r = Naming.lookup("rmi://10.0.0.13/TestRMI");
+      Remote r = Naming.lookup("rmi://localhost:1099/TestRMI");
       System.out.println(r);
       if (r instanceof Information) {
         String s = ((Information) r).getInformation();
